3e884cbe820f8e3195bbc8440d5af341276e18b3
ProTec Games <gamesprotec@gmail.com>
2025-12-19 10:20:50 +0000

    fix this ui

--- src/app/messages/layout.tsx
+++ src/app/messages/layout.tsx
@@ -17,8 +17,12 @@ export default function MessagesLayout({
   return (
     <div className="container mx-auto max-w-7xl h-full">
         <div className={cn("grid h-[calc(100vh-80px)] md:h-[calc(100vh-80px)]", isMobile ? "grid-cols-1" : "grid-cols-1 md:grid-cols-3 lg:grid-cols-4")}>
-            {isMobile ? children : list}
-            {!isMobile && children}
+            {isMobile ? children : (
+              <>
+                {list}
+                {children}
+              </>
+            )}
         </div>
     </div>
   );
